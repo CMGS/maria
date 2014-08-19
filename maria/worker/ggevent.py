83b6824221cd03ee45ce5428f5076b9f0321302a
waterdrinker <timedid@gmail.com>
2014-08-19 20:59:22 +0800

    Bug fix: gevent.py renamed ggevent.py.

--- maria/worker/ggevent.py
+++ maria/worker/ggevent.py
@@ -8,17 +8,12 @@ except ImportError:
 from gevent.monkey import patch_all
 patch_all(subprocess=False, aggressive=False)
 from gevent.server import StreamServer
-
 from maria.worker.base import WorkerClass
-import logging
 
 
-from importlib import import_module
-
-class GeventServer(object):
+class GeventServer(WorkerClass):
 
     def run(self):
-        self.logger = logging.getLogger(__name__)
         server = StreamServer(self.addr, self.app)
         try:
             self.logger.info('Maria System Start at %s:%s' % self.addr)
